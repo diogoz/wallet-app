be035e9493197bf6cf67bd43a25eed0754ff05c3
Diogo <dhesousa77@gmail.com>
2023-02-02 14:13:35 -0300

    Instalacao styled components + configuracao tema

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import {useFonts, Poppins_300Light, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold, Poppins_800ExtraBold } from '@expo-google-fonts/poppins'
 import {DMSerifDisplay_400Regular} from '@expo-google-fonts/dm-serif-display'
 import {DMSans_400Regular} from '@expo-google-fonts/dm-sans';
+import { ThemeProvider } from 'styled-components/native';
+import theme from "../src/styles/theme";
 export default function App() {
     const [fontsLoaded] = useFonts({
         Poppins_300Light, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold, Poppins_800ExtraBold, DMSerifDisplay_400Regular, DMSans_400Regular
@@ -13,10 +15,12 @@ export default function App() {
       }
 
     return (
+        <ThemeProvider theme={theme}>
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
         </View>
+        </ThemeProvider>
     );
 }
 
